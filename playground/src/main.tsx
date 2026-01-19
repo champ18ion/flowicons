53f5aac02463706bdb225ad5efd6bfbe98972069
Kunal Kumar <113764445+champ18ion@users.noreply.github.com>
2026-01-19 20:52:01 +0530

    feat: add stat18ion integration

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { init } from 'stat18ion'
+
+init({
+  siteId: '6cface3e-1177-409e-9191-d05e6404b0c7', 
+  debug: false,      // Set to true to see logs in console
+  trackLocal: false  // Set to true to send data from localhost/dev
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
